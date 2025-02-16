9bcf6f087578161845454730e4d2b7217cfa7dcb
Urk3L <urk3l@Rolands-MacBook-Pro.local>
2025-02-16 19:48:41 +0100

    configuration script for 63-22

---
+++ 63-22_labs/configure.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Update & Upgrade
+sudo apt update && sudo apt upgrade -y
+
+# Install packages
+sudo apt install -y firefox net-tools wireshark openssh-server python3 zsh git tmux vim \
+                   john sqlite3 nmap curl wget dsniff arduino
+
+# Enable SSH
+sudo systemctl enable --now ssh
+
+# Install Python tools
+python3 -m pip install --upgrade pip
+python3 -m pip install scapy flask
+
+# Install macof (part of dsniff)
+sudo apt install -y dsniff
+
+# Download RockYou wordlist
+if [ ! -f "/usr/share/wordlists/rockyou.txt" ]; then
+    sudo mkdir -p /usr/share/wordlists
+    sudo wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt -O /usr/share/wordlists/rockyou.txt
+fi
+
+
+sudo apt install -y wireshark
+
+sudo dpkg-reconfigure wireshark-common
+
+sudo usermod -aG wireshark $USER
+
+newgrp wireshark
+
+echo "Installation complete. Remember to manually install BurpSuite"
+
